cd9f086eca38a037225e3c8be87216bba63235b7
Arjun Patel <patel.arjun50@gmail.com>
2021-12-04 20:57:46 -0800

    Update index.js

--- pages/home/index.js
+++ pages/home/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/icon.png" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet" />
 
         <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2&display=swap" rel="stylesheet" />
